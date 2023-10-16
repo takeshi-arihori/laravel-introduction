15c3030917d9af37d404789fe196772b52606b4e
Takeshi Arihori <xxxarihoriixxx@gmail.com>
2023-10-16 21:21:19 +0900

    valudate query, validator

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use App\Http\Requests\HelloRequest;
+use Illuminate\Support\Facades\Validator;
+
 
 // global $head, $style, $body, $end;
 // $head = '<html><head>';
@@ -165,18 +167,44 @@ class HelloController extends Controller
     // Validation
     public function index(Request $request)
     {
-        return view('hello.index', ['msg' => 'フォームを入力:']);
+        // return view('hello.index', ['msg' => 'フォームを入力:']);
+
+        // クエリー文字列にバリデータを適用する
+        $validator = Validator::make($request->query(), [
+            'id' => 'required',
+            'pass' => 'required',
+        ]);
+        if ($validator->fails()) {
+            $msg = 'クエリーに問題があります。';
+        } else {
+            $msg = 'ID/PASSを受け付けました。フォームを入力してください。';
+        }
+        // hello?id=123&pass=abc などとurlに入力する
+        return view('hello.index', ['msg' => $msg]);
     }
 
-    public function post(HelloRequest $request)
+    public function post(Request $request)
     {
-        // フォームリクエスト作成後はコメントアウト
+        // フォームリクエスト作成後はコメントアウト 引数内を(HelloRequest $request)にする
         // $validate_rule = [
         //     'name' => 'required',
         //     'mail' => 'email',
         //     'age' => 'numeric|between:0,150'
         // ];
         // $this->validate($request, $validate_rule);
+
+        // バリデーションのカスタマイズ
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'mail' => 'email',
+            'age' => 'numeric|between:0,150'
+        ]);
+        if ($validator->fails()) {
+            return redirect('/hello')
+                ->withErrors($validator)
+                ->withInput();
+        }
+
         return view('hello.index', ['msg' => '正しく入力されました！']);
     }
 }
--- app/Http/Requests/HelloRequest.php
+++ app/Http/Requests/HelloRequest.php
@@ -31,4 +31,19 @@ class HelloRequest extends FormRequest
             'age' => 'numeric|between:0,150',
         ];
     }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array<string, string>
+     */
+    public function messages()
+    {
+        return [
+            'name.required' => '名前は必ず入力してください。',
+            'mail.email' => 'メールアドレスが必要です。',
+            'age.numeric' => '年齢を整数で記入ください。',
+            'age.between' => '年齢は0~150の間で入力ください。',
+        ];
+    }
 }
