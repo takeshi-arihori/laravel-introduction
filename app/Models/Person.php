02f330b7167c14e497d996d97d49f7241509a98b
Takeshi Arihori <xxxarihoriixxx@gmail.com>
2023-10-19 21:13:05 +0900

    scope person class

--- app/Models/Person.php
+++ app/Models/Person.php
@@ -2,27 +2,37 @@
 
 namespace App\Models;
 
+use App\Models\Scopes\ScopePerson;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 class Person extends Model
 {
-    public function getData()
+    /**
+     * モデルの「起動」メソッド
+     */
+    protected static function booted(): void
+    {
+        static::addGlobalScope(new ScopePerson);
+    }
+
+    public function getData(): string
     {
         return $this->id . ': ' . $this->name . ' (' . $this->age . ')';
     }
 
-    public function scopeNameEqual($query, $str)
+    public function scopeNameEqual($query, $str): Builder
     {
         return $query->where('name', $str);
     }
 
-    public function scopeAgeGreaterThan($query, $n)
+    public function scopeAgeGreaterThan($query, $n): Builder
     {
         return $query->where('age', '>=', $n);
     }
 
-    public function scopeAgeLessThan($query, $n)
+    public function scopeAgeLessThan($query, $n): Builder
     {
         return $query->where('age', '<=', $n);
     }
